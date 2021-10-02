a7b5070cfa96af1990341f6442cb84f4e75c492f
eske89 <63658249+eske89@users.noreply.github.com>
2021-10-02 18:04:24 +0200

    manage flights and passengers

--- logic/passengers.js
+++ logic/passengers.js
@@ -12,9 +12,48 @@ function Passengers() {
     }
    }
 
-   
+   function distributeAllSeatsToAllPassengers(vipPassengers, regularPassengers, numberOfFlights, businessSeatsPerFlight, economySeatsPerFlight){
+       let vipPassengersWithBusinessSeats = 0;
+       let vipPassengersWithEconomySeats = 0;
+       let regularPassengersWithBusinessSeats = 0;
+       let regularPassengersWithEconomySeats = 0;
 
-   return {checkFlightCapacity}
+       let totalBusinessSeats = businessSeatsPerFlight * numberOfFlights;
+       let totalEconomySeats = economySeatsPerFlight * numberOfFlights;
+
+       if (vipPassengers <= totalBusinessSeats) {
+            vipPassengersWithBusinessSeats = vipPassengers;
+            vipPassengersWithEconomySeats = 0;
+            totalBusinessSeats = totalBusinessSeats - vipPassengersWithBusinessSeats;
+       } else {
+            vipPassengersWithBusinessSeats = totalBusinessSeats;
+            vipPassengersWithEconomySeats = vipPassengers - vipPassengersWithBusinessSeats;
+            totalBusinessSeats = 0;
+            totalEconomySeats = totalEconomySeats - vipPassengersWithEconomySeats;
+       }
+       if  (regularPassengers <= totalBusinessSeats) {
+           regularPassengersWithBusinessSeats = regularPassengers;
+           regularPassengersWithEconomySeats = 0;
+       } else {
+           regularPassengersWithBusinessSeats = totalBusinessSeats;
+           let passengersThatShouldSitOnEconomySeats = regularPassengers - regularPassengersWithBusinessSeats;
+           if (passengersThatShouldSitOnEconomySeats <= totalEconomySeats) {
+               regularPassengersWithEconomySeats = passengersThatShouldSitOnEconomySeats;
+           } else {
+               regularPassengersWithEconomySeats = totalEconomySeats;
+           }
+
+       }
+
+       return {
+           vipPassengersWithBusinessSeats,
+           vipPassengersWithEconomySeats,
+           regularPassengersWithBusinessSeats,
+           regularPassengersWithEconomySeats
+       }
+   }
+
+   return {checkFlightCapacity, distributeAllSeatsToAllPassengers}
 }
 
 module.exports = Passengers();
--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,25 @@
 function Util() {
+    function calculateTotalDistributedPassengers(distributedPassengers) {
+        let { vipPassengersWithBusinessSeats,
+            vipPassengersWithEconomySeats,
+            regularPassengersWithBusinessSeats,
+            regularPassengersWithEconomySeats } = distributedPassengers;
+        
+        return vipPassengersWithBusinessSeats +
+            vipPassengersWithEconomySeats +
+            regularPassengersWithBusinessSeats +
+            regularPassengersWithEconomySeats
+    }
 
+function calculateTotalNumberOfPassengers(passengersNumber){
+    let totalPassengers = 0;
+    for(let i = 0; i < passengersNumber.length; i++) {
+        totalPassengers += passengersNumber[i];
+    }
+    return totalPassengers
 }
 
+    return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers}
+}
+
+module.exports = Util();
