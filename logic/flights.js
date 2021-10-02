55ab90572e6e38d121048844d37347b3b49f3155
eske89 <63658249+eske89@users.noreply.github.com>
2021-10-02 17:19:08 +0200

    flights module done

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,35 @@
 function Flights() {
+    function calculateNumberOfFlights(passengers, capacity){
+        if (passengers < 0) {
+            throw new Error("The number of passengers must be a positive integer value")
+        }
+        if (capacity < 0) {
+            throw new Error("The number of capacity must be a positive integer value")
+        }
+        return Math.ceil(passengers / capacity)
+    }
 
+    function checkAircraftRevision(distanceLimit, distancesArray) {
+            let totalDistance = 0;
+            for(let i = 0; i < distancesArray.length; i++) {
+                totalDistance += distancesArray[i];
+            }
+
+            if (totalDistance <= distanceLimit/2){
+                return "The revision needs to be done within the next 3 months"
+            }
+            else if (totalDistance <= distanceLimit*0.75){
+                return "The revision needs to be done within the next 2 months"
+            }
+            else if (totalDistance <= distanceLimit){
+                return "The revision needs to be done within the next month"
+            }
+            else {
+                throw new Error()
+            }
+    }
+
+    return {calculateNumberOfFlights, checkAircraftRevision};
 }
 
+module.exports = Flights();
--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,21 @@
 function Passengers() {
+   function checkFlightCapacity(capacity, passengersNumber){
+    let totalPassengers = 0;
+    for(let i = 0; i < passengersNumber.length; i++) {
+        totalPassengers += passengersNumber[i];
+    }
+    if (totalPassengers < capacity){
+        return totalPassengers
+    }
+    else {
+        throw new Error()
+    }
+   }
 
+   
+
+   return {checkFlightCapacity}
 }
+
+module.exports = Passengers();
 
