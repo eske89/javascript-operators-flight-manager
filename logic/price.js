e138d462a51ea406190ac744596c878fb4f910eb
eske89 <63658249+eske89@users.noreply.github.com>
2021-10-05 20:28:30 +0200

    Manage prices and distances

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,37 @@
 function Prices() {
+    function calculateFinalPrice(basePrice, variationPassengerType, variationFlightType){
+        basePrice = parseFloat(basePrice);
+        variationPassengerType = parseFloat(variationPassengerType);
+        variationFlightType = parseFloat(variationFlightType);
+        
+        //  100 - 5% * 100 - 3% * (100 - 5% * 100) = 92.15
+        let passengerPrice = basePrice + basePrice * (variationPassengerType / 100);
+        let flightPrice = passengerPrice + passengerPrice * (variationFlightType / 100);
+        return flightPrice.toFixed(2);
+    }
 
+    function calculateDefaultFinalPrice(basePrice, passengerType, flightType){
+        let passengerReductions = {
+            regular: -5,
+            vip: 5
+        };
+        let flightReductions = {
+            economy: -3,
+            business: 10
+        };
+
+        return calculateFinalPrice(
+            parseFloat(basePrice),
+            passengerReductions[passengerType.toLowerCase()],
+            flightReductions[flightType.toLowerCase()]
+        );
+    }
+
+    function calculateTotalFinalPrice(numberOfSeats, passengerType, flightType, basePrice){
+        return calculateDefaultFinalPrice(basePrice, passengerType, flightType) * numberOfSeats
+    }
+
+    return {calculateFinalPrice, calculateDefaultFinalPrice, calculateTotalFinalPrice};
 }
 
+module.exports = Prices();
--- logic/util.js
+++ logic/util.js
@@ -11,15 +11,35 @@ function Util() {
             regularPassengersWithEconomySeats
     }
 
-function calculateTotalNumberOfPassengers(passengersNumber){
-    let totalPassengers = 0;
-    for(let i = 0; i < passengersNumber.length; i++) {
-        totalPassengers += passengersNumber[i];
+    function calculateTotalNumberOfPassengers(passengersNumber){
+        let totalPassengers = 0;
+        for(let p of passengersNumber) {
+            totalPassengers += p;
+        }
+        return totalPassengers
     }
-    return totalPassengers
-}
 
-    return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers}
+    function checkInput(input){
+        if (input == false || isNaN(input)) {
+            throw new Error()
+        }
+    }
+
+    function calculateTotalDistance(distances){
+        let totalDistance = 0;
+        for(let d of distances) {
+            if (d > 0) {
+                totalDistance += d;
+            }
+        }
+        return totalDistance
+    }
+
+    function calculateBonusPoints(distanceBusiness, distanceEconomy, businessBonus, economyBonus){
+        return calculateTotalDistance(distanceBusiness) * businessBonus/100 + calculateTotalDistance(distanceEconomy) * economyBonus/100
+    }
+
+    return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers, checkInput, calculateTotalDistance, calculateBonusPoints}
 }
 
 module.exports = Util();
